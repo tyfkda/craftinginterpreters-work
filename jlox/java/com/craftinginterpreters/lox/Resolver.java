409a0ef54680d4ca0491a616c75e4f9ba7bb3406
tyfkda <tyfkda@gmail.com>
2019-12-08 05:34:07 +0900

    12.4 Methods on Classes

--- jlox/java/com/craftinginterpreters/lox/Resolver.java
+++ jlox/java/com/craftinginterpreters/lox/Resolver.java
@@ -9,6 +9,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   private enum FunctionType {
     NONE,
     FUNCTION,
+    METHOD,
   }
 
   private final Interpreter interpreter;
@@ -43,6 +44,12 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   public Void visitClassStmt(Stmt.Class stmt) {
     declare(stmt.name);
     define(stmt.name);
+
+    for (Stmt.Function method : stmt.methods) {
+      FunctionType declaration = FunctionType.METHOD;
+      resolveFunction(method, declaration);
+    }
+
     return null;
   }
 

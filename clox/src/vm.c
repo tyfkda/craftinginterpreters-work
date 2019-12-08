429a5f0c85ea356ed0ae5aefd0c2e8e5a4ba22a0
tyfkda <tyfkda@gmail.com>
2019-12-08 16:16:01 +0900

    Chap 15.3 An Arithmetic Calculator

--- clox/src/vm.c
+++ clox/src/vm.c
@@ -20,6 +20,12 @@ void freeVM(void) {
 static InterpretResult run(void) {
 #define READ_BYTE()  (*vm.ip++)
 #define READ_CONSTANT()  (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+  do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+  } while (false)
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -40,6 +46,11 @@ static InterpretResult run(void) {
       push(constant);
       break;
     }
+    case OP_ADD:      BINARY_OP(+); break;
+    case OP_SUBTRACT: BINARY_OP(-); break;
+    case OP_MULTIPLY: BINARY_OP(*); break;
+    case OP_DIVIDE:   BINARY_OP(/); break;
+    case OP_NEGATE:   push(-pop()); break;
     case OP_RETURN:
       printValue(pop());
       printf("\n");
@@ -49,6 +60,7 @@ static InterpretResult run(void) {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk) {

fd89af69f1d25a6197bab24dd52de2022bb6cc38
tyfkda <tyfkda@gmail.com>
2019-12-08 15:59:13 +0900

    Chap 15.2 A Value Stack Manipulator

--- clox/src/vm.c
+++ clox/src/vm.c
@@ -6,7 +6,12 @@
 
 VM vm;
 
+static void resetStack(void) {
+  vm.stackTop = vm.stack;
+}
+
 void initVM(void) {
+  resetStack();
 }
 
 void freeVM(void) {
@@ -18,6 +23,13 @@ static InterpretResult run(void) {
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
+    printf("          ");
+    for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("[ ");
+      printValue(*slot);
+      printf(" ]");
+    }
+    printf("\n");
     disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
 
@@ -25,11 +37,12 @@ static InterpretResult run(void) {
     switch (instruction = READ_BYTE()) {
     case OP_CONSTANT: {
       Value constant = READ_CONSTANT();
-      printValue(constant);
-      printf("\n");
+      push(constant);
       break;
     }
     case OP_RETURN:
+      printValue(pop());
+      printf("\n");
       return INTERPRET_OK;
     }
   }
@@ -42,4 +55,14 @@ InterpretResult interpret(Chunk* chunk) {
   vm.chunk = chunk;
   vm.ip = vm.chunk->code;
   return run();
+}
+
+void push(Value value) {
+  *vm.stackTop = value;
+  ++vm.stackTop;
+}
+
+Value pop(void) {
+  --vm.stackTop;
+  return *vm.stackTop;
 }

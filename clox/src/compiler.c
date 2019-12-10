63e39ee76485913672bc02d61147c9e7c18da0a5
tyfkda <tyfkda@gmail.com>
2019-12-10 09:23:53 +0900

    Chap 19 Strings
    19.1 Values and Objects
    19.2 Struct Inheritance
    19.3 Strings
    19.3 Operation on Strings

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -3,6 +3,7 @@
 
 #include "common.h"
 #include "compiler.h"
+#include "object.h"
 #include "scanner.h"
 
 #ifdef DEBUG_PRINT_CODE
@@ -144,6 +145,11 @@ static void number(void) {
   emitConstant(NUMBER_VAL(value));
 }
 
+static void string(void) {
+  emitConstant(OBJ_VAL(copyString(parser.previous.start + 1,
+                                  parser.previous.length - 2)));
+}
+
 static void binary(void) {
   // Remember the operator.
   TokenType operatorType = parser.previous.type;
@@ -215,7 +221,7 @@ ParseRule rules[] = {
   { NULL,     binary,  PREC_EQUALITY },   // TOKEN_LESS
   { NULL,     binary,  PREC_EQUALITY },   // TOKEN_LESS_EQUAL
   { NULL,     NULL,    PREC_NONE },       // TOKEN_IDENTIFIER
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_STRING
+  { string,   NULL,    PREC_NONE },       // TOKEN_STRING
   { number,   NULL,    PREC_NONE },       // TOKEN_NUMBER
   { NULL,     NULL,    PREC_NONE },       // TOKEN_AND
   { NULL,     NULL,    PREC_NONE },       // TOKEN_CLASS

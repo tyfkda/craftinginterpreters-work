5290f012e99cb9f7d9a5e09a05bbd9e775a180c4
tyfkda <tyfkda@gmail.com>
2019-12-14 11:54:42 +0900

    Chap 22 Local Variables
    22.1 Representing Local Variables
    22.2 Block Statements
    22.3 Declaring Local Variables
    22.4 Using Locals
    22.5 Another Scope Edge Case

--- clox/src/vm.c
+++ clox/src/vm.c
@@ -113,6 +113,18 @@ static InterpretResult run(void) {
     case OP_FALSE:  push(BOOL_VAL(false)); break;
     case OP_POP:  pop(); break;
 
+    case OP_GET_LOCAL: {
+      uint8_t slot = READ_BYTE();
+      push(vm.stack[slot]);
+      break;
+    }
+
+    case OP_SET_LOCAL: {
+      uint8_t slot = READ_BYTE();
+      vm.stack[slot] = peek(0);
+      break;
+    }
+
     case OP_GET_GLOBAL: {
       ObjString* name = READ_STRING();
       Value value;
